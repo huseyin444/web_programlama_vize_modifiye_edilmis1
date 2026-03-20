ca0c6b256e6c0c029660a23645b33d76bf849537
huseyin444 <huseyinozcan789@gmail.com>
2026-03-20 23:07:52 +0300

    Bolum 3 tamamlandi: Gorunumler (Views) ve Sablonlar (Templates) eklendi

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "quiz"
 urlpatterns = [
+    # ex: /quiz/
     path("", views.index, name="index"),
+    # ex: /quiz/5/
+    path("<int:soru_id>/", views.detay, name="detay"),
+    # ex: /quiz/5/sonuclar/
+    path("<int:soru_id>/sonuclar/", views.sonuclar, name="sonuclar"),
+    # ex: /quiz/5/cevapla/
+    path("<int:soru_id>/cevapla/", views.cevapla, name="cevapla"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,20 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Soru
 
 def index(request):
-    return HttpResponse("Merhaba! Bilgi Yarışması projemizin ilk sayfasına hoş geldiniz.")
+    son_sorular = Soru.objects.order_by("-yayinlanma_tarihi")[:5]
+    context = {"son_sorular": son_sorular}
+    return render(request, "quiz/index.html", context)
+
+def detay(request, soru_id):
+    # Soru bulunamazsa otomatik olarak 404 hata sayfası gösterir
+    soru = get_object_or_404(Soru, pk=soru_id)
+    return render(request, "quiz/detay.html", {"soru": soru})
+
+def sonuclar(request, soru_id):
+    response = "Soru %s sonuçlarına bakıyorsunuz."
+    return HttpResponse(response % soru_id)
+
+def cevapla(request, soru_id):
+    return HttpResponse("Soru %s için cevap gönderiyorsunuz." % soru_id)
