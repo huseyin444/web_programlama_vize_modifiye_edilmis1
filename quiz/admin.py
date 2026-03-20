f5fae38fc85dd61e23f7b665f9169261625ab23f
huseyin444 <huseyinozcan789@gmail.com>
2026-03-20 22:38:17 +0300

    Bolum 2 tamamlandi: Soru ve Secenek modelleri eklendi, admin paneli ayarlandi

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Soru, Secenek
 
-# Register your models here.
+admin.site.register(Soru)
+admin.site.register(Secenek)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Soru(models.Model):
+    soru_metni = models.CharField(max_length=250)
+    yayinlanma_tarihi = models.DateTimeField("yayınlanma tarihi")
+
+    def __str__(self):
+        return self.soru_metni
+
+    def yeni_mi(self):
+        return self.yayinlanma_tarihi >= timezone.now() - datetime.timedelta(days=1)
+
+class Secenek(models.Model):
+    soru = models.ForeignKey(Soru, on_delete=models.CASCADE)
+    secenek_metni = models.CharField(max_length=100)
+    dogru_mu = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.secenek_metni
